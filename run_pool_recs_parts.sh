2292bda125b361460eb5ab71f12427e09956786f
Gustavo Escobedo <gescobedopr@gmail.com>
2024-03-20 09:07:32 +0100

    improvede funtions

--- run_pool_recs_parts.sh
+++ run_pool_recs_parts.sh
@@ -15,8 +15,39 @@ conda activate recbole-bert
 
 
 # LFM  MULTVAE
-tmux new -d 'export CUDA_VISIBLE_DEVICES=0 ; python pool_recs.py --model MultiVAE --dataset lfm-100k --data_path /share/hel/datasets/cf_obfuscation/ --out_dir /home/gustavoe/obfuscation/all_test_mean_stereo/ --datasets_file /home/gustavoe/all_test_mean_stereo/lfm-100k/datasets.part0.json --nproc 2 --gpu 1'
-tmux new -d 'export CUDA_VISIBLE_DEVICES=1 ; python pool_recs.py --model MultiVAE --dataset lfm-100k --data_path /share/hel/datasets/cf_obfuscation/ --out_dir /home/gustavoe/obfuscation/all_test_mean_stereo/ --datasets_file /home/gustavoe/all_test_mean_stereo/lfm-100k/datasets.part1.json --nproc 2 --gpu 1'
-tmux new -d 'export CUDA_VISIBLE_DEVICES=2 ; python pool_recs.py --model MultiVAE --dataset lfm-100k --data_path /share/hel/datasets/cf_obfuscation/ --out_dir /home/gustavoe/obfuscation/all_test_mean_stereo/ --datasets_file /home/gustavoe/all_test_mean_stereo/lfm-100k/datasets.part2.json --nproc 2 --gpu 1'
-tmux new -d 'export CUDA_VISIBLE_DEVICES=3 ; python pool_recs.py --model MultiVAE --dataset lfm-100k --data_path /share/hel/datasets/cf_obfuscation/ --out_dir /home/gustavoe/obfuscation/all_test_mean_stereo/ --datasets_file /home/gustavoe/all_test_mean_stereo/lfm-100k/datasets.part3.json --nproc 2 --gpu 1'
-    
+#tmux new -d 'export CUDA_VISIBLE_DEVICES=0 ; python pool_recs.py --model MultiVAE --dataset lfm-100k --data_path /share/hel/datasets/cf_obfuscation/ --out_dir /home/gustavoe/obfuscation/all_test_mean_stereo/ --datasets_file /home/gustavoe/all_test_mean_stereo/lfm-100k/datasets.part0.json --nproc 2 --gpu 1'
+#tmux new -d 'export CUDA_VISIBLE_DEVICES=1 ; python pool_recs.py --model MultiVAE --dataset lfm-100k --data_path /share/hel/datasets/cf_obfuscation/ --out_dir /home/gustavoe/obfuscation/all_test_mean_stereo/ --datasets_file /home/gustavoe/all_test_mean_stereo/lfm-100k/datasets.part1.json --nproc 2 --gpu 1'
+#tmux new -d 'export CUDA_VISIBLE_DEVICES=2 ; python pool_recs.py --model MultiVAE --dataset lfm-100k --data_path /share/hel/datasets/cf_obfuscation/ --out_dir /home/gustavoe/obfuscation/all_test_mean_stereo/ --datasets_file /home/gustavoe/all_test_mean_stereo/lfm-100k/datasets.part2.json --nproc 2 --gpu 1'
+#tmux new -d 'export CUDA_VISIBLE_DEVICES=3 ; python pool_recs.py --model MultiVAE --dataset lfm-100k --data_path /share/hel/datasets/cf_obfuscation/ --out_dir /home/gustavoe/obfuscation/all_test_mean_stereo/ --datasets_file /home/gustavoe/all_test_mean_stereo/lfm-100k/datasets.part3.json --nproc 2 --gpu 1'
+
+# ML1m MULiTVAE
+#tmux new -d 'export CUDA_VISIBLE_DEVICES=0 ; python pool_recs.py --model MultiVAE --dataset ml-1m --data_path /share/hel/datasets/cf_obfuscation/ --out_dir /home/gustavoe/obfuscation/all_test_mean_stereo/ --datasets_file /home/gustavoe/all_test_mean_stereo/ml-1m/datasets.part0.json --nproc 4 --gpu 1'
+#tmux new -d 'export CUDA_VISIBLE_DEVICES=1 ; python pool_recs.py --model MultiVAE --dataset ml-1m --data_path /share/hel/datasets/cf_obfuscation/ --out_dir /home/gustavoe/obfuscation/all_test_mean_stereo/ --datasets_file /home/gustavoe/all_test_mean_stereo/ml-1m/datasets.part1.json --nproc 4 --gpu 1'
+#tmux new -d 'export CUDA_VISIBLE_DEVICES=2 ; python pool_recs.py --model MultiVAE --dataset ml-1m --data_path /share/hel/datasets/cf_obfuscation/ --out_dir /home/gustavoe/obfuscation/all_test_mean_stereo/ --datasets_file /home/gustavoe/all_test_mean_stereo/ml-1m/datasets.part2.json --nproc 4 --gpu 1'
+#tmux new -d 'export CUDA_VISIBLE_DEVICES=3 ; python pool_recs.py --model MultiVAE --dataset ml-1m --data_path /share/hel/datasets/cf_obfuscation/ --out_dir /home/gustavoe/obfuscation/all_test_mean_stereo/ --datasets_file /home/gustavoe/all_test_mean_stereo/ml-1m/datasets.part3.json --nproc 4 --gpu 1'
+
+# ligtGCN originals
+#tmux new -d 'export CUDA_VISIBLE_DEVICES=0 ; python pool_recs.py --model LightGCN --dataset ml-1m --data_path /share/hel/datasets/cf_obfuscation/ --out_dir /home/gustavoe/obfuscation/all_test_mean_stereo/ --datasets_file /home/gustavoe/all_test_mean_stereo/ml-1m/datasets.original.part0.json --nproc 1 --gpu 1'
+#tmux new -d 'export CUDA_VISIBLE_DEVICES=0 ; python pool_recs.py --model LightGCN --dataset lfm-100k --data_path /share/hel/datasets/cf_obfuscation/ --out_dir /home/gustavoe/obfuscation/all_test_mean_stereo/ --datasets_file /home/gustavoe/all_test_mean_stereo/lfm-100k/datasets.original.part0.json --nproc 1 --gpu 1'
+
+# ML1m lightGCN imputate
+#tmux new -d 'export CUDA_VISIBLE_DEVICES=1 ; python pool_recs.py --model LightGCN --dataset ml-1m --data_path /share/hel/datasets/cf_obfuscation/ --out_dir /home/gustavoe/obfuscation/all_test_mean_stereo/ --datasets_file /home/gustavoe/all_test_mean_stereo/ml-1m/datasets.imputate.part0.json --nproc 2 --gpu 1'
+#tmux new -d 'export CUDA_VISIBLE_DEVICES=2 ; python pool_recs.py --model LightGCN --dataset ml-1m --data_path /share/hel/datasets/cf_obfuscation/ --out_dir /home/gustavoe/obfuscation/all_test_mean_stereo/ --datasets_file /home/gustavoe/all_test_mean_stereo/ml-1m/datasets.imputate.part1.json --nproc 2 --gpu 1'
+#tmux new -d 'export CUDA_VISIBLE_DEVICES=3 ; python pool_recs.py --model LightGCN --dataset ml-1m --data_path /share/hel/datasets/cf_obfuscation/ --out_dir /home/gustavoe/obfuscation/all_test_mean_stereo/ --datasets_file /home/gustavoe/all_test_mean_stereo/ml-1m/datasets.imputate.part2.json --nproc 2 --gpu 1'
+
+# ML1m lightGCN weighted
+#tmux new -d 'export CUDA_VISIBLE_DEVICES=1 ; python pool_recs.py --model LightGCN --dataset ml-1m --data_path /share/hel/datasets/cf_obfuscation/ --out_dir /home/gustavoe/obfuscation/all_test_mean_stereo/ --datasets_file /home/gustavoe/all_test_mean_stereo/ml-1m/datasets.weighted.part0.json --nproc 2 --gpu 1'
+#tmux new -d 'export CUDA_VISIBLE_DEVICES=2 ; python pool_recs.py --model LightGCN --dataset ml-1m --data_path /share/hel/datasets/cf_obfuscation/ --out_dir /home/gustavoe/obfuscation/all_test_mean_stereo/ --datasets_file /home/gustavoe/all_test_mean_stereo/ml-1m/datasets.weighted.part1.json --nproc 2 --gpu 1'
+#tmux new -d 'export CUDA_VISIBLE_DEVICES=3 ; python pool_recs.py --model LightGCN --dataset ml-1m --data_path /share/hel/datasets/cf_obfuscation/ --out_dir /home/gustavoe/obfuscation/all_test_mean_stereo/ --datasets_file /home/gustavoe/all_test_mean_stereo/ml-1m/datasets.weighted.part2.json --nproc 2 --gpu 1'
+
+
+#PENDING
+# LFM lightGCN weighted
+#tmux new -d 'export CUDA_VISIBLE_DEVICES=0 ; python pool_recs.py --model LightGCN --dataset lfm-100k --data_path /share/hel/datasets/cf_obfuscation/ --out_dir /home/gustavoe/obfuscation/all_test_mean_stereo/ --datasets_file /home/gustavoe/all_test_mean_stereo/lfm-100k/datasets.weighted.part0.json --nproc 2 --gpu 1'
+tmux new -d 'export CUDA_VISIBLE_DEVICES=1 ; python pool_recs.py --model LightGCN --dataset lfm-100k --data_path /share/hel/datasets/cf_obfuscation/ --out_dir /home/gustavoe/obfuscation/all_test_mean_stereo/ --datasets_file /home/gustavoe/all_test_mean_stereo/lfm-100k/datasets.weighted.part1.json --nproc 2 --gpu 1'
+tmux new -d 'export CUDA_VISIBLE_DEVICES=2 ; python pool_recs.py --model LightGCN --dataset lfm-100k --data_path /share/hel/datasets/cf_obfuscation/ --out_dir /home/gustavoe/obfuscation/all_test_mean_stereo/ --datasets_file /home/gustavoe/all_test_mean_stereo/lfm-100k/datasets.weighted.part2.json --nproc 2 --gpu 1'
+
+# LFM lightGCN imputate
+tmux new -d 'export CUDA_VISIBLE_DEVICES=3 ; python pool_recs.py --model LightGCN --dataset lfm-100k --data_path /share/hel/datasets/cf_obfuscation/ --out_dir /home/gustavoe/obfuscation/all_test_mean_stereo/ --datasets_file /home/gustavoe/all_test_mean_stereo/lfm-100k/datasets.imputate.part0.json --nproc 2 --gpu 1'
+#tmux new -d 'export CUDA_VISIBLE_DEVICES=1 ; python pool_recs.py --model LightGCN --dataset lfm-100k --data_path /share/hel/datasets/cf_obfuscation/ --out_dir /home/gustavoe/obfuscation/all_test_mean_stereo/ --datasets_file /home/gustavoe/all_test_mean_stereo/lfm-100k/datasets.imputate.part1.json --nproc 2 --gpu 1'
+#tmux new -d 'export CUDA_VISIBLE_DEVICES=2 ; python pool_recs.py --model LightGCN --dataset lfm-100k --data_path /share/hel/datasets/cf_obfuscation/ --out_dir /home/gustavoe/obfuscation/all_test_mean_stereo/ --datasets_file /home/gustavoe/all_test_mean_stereo/lfm-100k/datasets.imputate.part2.json --nproc 2 --gpu 1'
